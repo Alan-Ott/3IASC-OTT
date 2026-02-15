eff248ad91635141e70190c578209debb6866439
Alan Ott <alan.ott@ecole-hexagone.com>
2026-02-15 19:31:51 +0100

    commit

--- roles/postgresql/templates/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS instructions (
-    id SERIAL PRIMARY KEY,
-    message TEXT NOT NULL,
-    created_at TIMESTAMPTZ DEFAULT NOW()
-);
-
-INSERT INTO instructions (message) VALUES 
-('Réalise le docker-compose et branche le front sur le back. Bonne chance !');
